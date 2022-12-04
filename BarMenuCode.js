4d8c10831da27bb993a85509915a645a4466c3de
Krishna Chaitanya Kommera <kommerakrishnachaitanya@gmail.com>
2022-12-04 12:33:39 +0530

    added minifiedVersion

---
+++ BarMenuCode.js
@@ -0,0 +1,14 @@
+
+const prevBtnBar = document.querySelector("#prev-btn-bar");
+const nextBtnBar = document.querySelector("#next-btn-bar");
+const bookBar = document.querySelector("#book-bar");
+const pageIndicatorBar = document.querySelector('#page-indicator-bar');
+const papers= document.getElementsByClassName('paper');
+const myDocument = document.querySelector('#menu-container-bar');
+const nextButtonBar = document.querySelector('#next-button-bar');
+const prevButtonBar = document.querySelector('#prev-button-bar');
+const zoomInBar = document.querySelector('#zoom-in-bar');
+const zoomOutBar = document.querySelector('#zoom-out-bar');
+const fullScreenBar = document.querySelector('#full-screen-bar');
+const controllerBar = document.querySelector('#controller-bar');
+const mainContainer = document.getElementsByClassName('main-container')[0];
